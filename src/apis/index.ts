bf8d6cba0e6f35601979fd932f6612689a3c164e
devfrank9 <devfrank9@gmail.com>
2022-09-14 17:46:24 +0900

    ✨ Feat : api 공통 함수

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,18 @@
 import axios from 'axios';
+
+const apiRoot = axios.create({
+  method: 'get',
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: { Authorization: 'token ' + process.env.REACT_APP_API_KEY },
+  params: { state: 'open', sort: 'comments', direction: 'desc' },
+});
+
+export const getIssues = async (pageNumber: number) => {
+  const response = await apiRoot('/issues', { params: { page: pageNumber } });
+  return response.data;
+};
+
+export const getIssue = async (id: number) => {
+  const response = await apiRoot(`issues${id}`);
+  return response.data;
+};
