fa6f7b9d4f1bc477330f9d17d6815cf42789b29b
MINYUKYUNG <minew0223@gmail.com>
2022-09-14 18:44:06 +0900

    ⚡️ Feature : Context API 로 issue 페이지 관리 코드 생성

---
+++ src/store/IssuesStore.tsx
@@ -0,0 +1,44 @@
+import { createContext, useState } from 'react';
+import { OWNER, REPO, issuesApi } from '../apis/index';
+
+interface issueGuard {
+  [key: string]: any
+};
+
+interface storeGuard {
+  owner: string;
+  repo: string;
+  issueLists: issueGuard[];
+  scrollLists: (page: number) => Promise<null | void>;
+  setIssueLists: React.Dispatch<React.SetStateAction<issueGuard[]>>;
+  get: () => any;
+  set: (value: any) => void;
+};
+
+export const IssuesContext = createContext<storeGuard | null>(null);
+
+export default function IssuesStore(props: { children: any}) {
+  const [issueLists, setIssueLists] = useState<issueGuard[]>([]);
+  const owner = OWNER, repo = REPO, callApi = issuesApi;
+  
+  const bridge = {
+    owner,
+    repo,
+    issueLists,
+    scrollLists: async (page: number) => {
+      const addLists = await callApi(page);
+      setIssueLists([ ...issueLists, ...addLists ]);
+    },
+    setIssueLists,
+
+    get: () => {
+      const result = JSON.parse(sessionStorage.getItem('issue_lists') || '[]');
+      return result;
+    },
+    set: (value: any) => sessionStorage.setItem('issue_lists', JSON.stringify(value))
+  };
+
+  return (
+    <IssuesContext.Provider value={ bridge }>{ props.children }</IssuesContext.Provider>
+  );
+};
