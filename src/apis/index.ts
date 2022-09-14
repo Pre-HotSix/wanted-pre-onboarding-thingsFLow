9ff4b2e634d4d61eadbd6a3521d5c3a1650203ef
MINYUKYUNG <minew0223@gmail.com>
2022-09-14 18:45:26 +0900

    ⚡️ Feature : api 관리 파일 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+const ACCESS_TOKEN = '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';
+const OWNER = 'angular';
+const REPO = 'angular-cli';
+const STATE = 'open';
+const SORT = 'comments';
+
+const issuesApi = async (page: number) => {
+  const response = await axios.get(`https://api.github.com/repos/${OWNER}/${REPO}/issues?state=${STATE}&sort=${SORT}&page=${page}`, 
+  {
+    headers: { 
+      'Accept': 'application/vnd.github+json',
+      'Authorization': `Bearer ${ACCESS_TOKEN}`
+    },
+  });
+
+  return response.data;
+};
+
+export { OWNER, REPO, issuesApi };
