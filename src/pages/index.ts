745dbaad51e45589b99c8403b234288c764f4ace
MINYUKYUNG <minew0223@gmail.com>
2022-09-14 18:44:59 +0900

    🎨 Modify : 파일 라우터 구성 변경

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
-export { default as HomePage } from './mainPage/mainPage';
+export { default as MainPage } from './MainPage';
+export { default as DetailPage } from './DetailPage';
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../components/commons/layout';
-import MainPage from '../pages/mainPage/mainPage';
+import { MainPage, DetailPage } from '../pages/index';
 
 export default function Router() {
   return (
@@ -8,6 +8,7 @@ export default function Router() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<MainPage />} />
+          <Route path="/:id" element={<DetailPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
